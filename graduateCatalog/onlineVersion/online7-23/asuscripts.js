714fc53f32115558d4cc0af6b8f58cb1ec26b71a
whu-asurams <whu@asurams.edu>
2023-09-19 23:06:23 -0400

    update

---
+++ graduateCatalog/onlineVersion/online7-23/asuscripts.js
@@ -0,0 +1,31 @@
+ function show_hut11(){
+          document.getElementById("hut11").innerHTML="";
+   }    
+function show_hut12(){
+          document.getElementById("hut12").innerHTML="";
+   }
+
+ function show_hut13(){
+          document.getElementById("hut13").innerHTML="";
+   }
+
+
+
+ function show_hut21(){
+          document.getElementById("hut21").innerHTML="";
+   }
+
+ function show_hut23(){
+          document.getElementById("hut23").innerHTML="";
+   }
+
+
+ function show_hut31(){
+          document.getElementById("hut31").innerHTML="";
+   }
+ function show_hut32(){
+          document.getElementById("hut32").innerHTML="";
+   }
+ function show_hut33(){
+          document.getElementById("hut33").innerHTML="";
+   }
